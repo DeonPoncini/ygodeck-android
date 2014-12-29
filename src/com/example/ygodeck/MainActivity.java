787606c3c8496c8fefa38a3d49084e0fe601bd5e
Deon Poncini <dex1337@gmail.com>
2014-12-29 13:49:41 -0800

    Example android application using jni library
    The example application now uses the ygodeck jni library
    
    The library is packaged up as a jar, and the application imports the jar
    and can call native functions on it.
    
    This means all the build stages are in place to write a java wrapper
    around the c wrapper to allow java clients to use the ygodeck library

--- src/com/example/ygodeck/MainActivity.java
+++ src/com/example/ygodeck/MainActivity.java
@@ -4,18 +4,39 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import net.sectorsoftware.ygodeck.Example;
 
 public class MainActivity extends ActionBarActivity {
+
+    static {
+        System.loadLibrary("ygodeck-jni");
+    }
+
+    private Example mExample = new Example();
+    private int mCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button increment = (Button) findViewById(R.id.button);
+        increment.setOnClickListener(mOnIncrement);
     }
 
+    private View.OnClickListener mOnIncrement = new View.OnClickListener() {
 
-    @Override
+        @Override
+        public void onClick(View v) {
+            mCounter = mExample.increment(mCounter);
+            Toast.makeText(MainActivity.this, Integer.toString(mCounter), Toast.LENGTH_SHORT).show();
+        }
+    };
+
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
