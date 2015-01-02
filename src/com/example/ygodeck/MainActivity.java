3e83326297692105cf6c8504ab5c9ef6cd63cfa7
Deon Poncini <dex1337@gmail.com>
2015-01-02 15:04:06 -0800

    Start java binding with android app test
    Add the DB and Cardselector java bindings, and modify the android
    application on the way to test the features
    
    Add the card.db file that has been generated as a raw resource, in the
    future build this elsewhere
    
    To open in Android studio requires a gradle.properties file that points
    at where the libraryPath is for the java binding JAR, which then causes
    the cmake build to not work correctly, have to investigate a better
    method of allowing android studio to work

--- src/com/example/ygodeck/MainActivity.java
+++ src/com/example/ygodeck/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.ygodeck;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import net.sectorsoftware.ygodeck.Example;
+import net.sectorsoftware.ygo.deck.DB;
+import net.sectorsoftware.ygo.deck.CardSelector;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -16,8 +30,9 @@ public class MainActivity extends ActionBarActivity {
         System.loadLibrary("ygodeck-jni");
     }
 
-    private Example mExample = new Example();
-    private int mCounter = 0;
+    private ListView mListView;
+    ArrayList<String> mListItems = new ArrayList<String>();
+    ArrayAdapter<String> mListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +41,58 @@ public class MainActivity extends ActionBarActivity {
 
         Button increment = (Button) findViewById(R.id.button);
         increment.setOnClickListener(mOnIncrement);
+
+        mListView = (ListView) findViewById(R.id.listView);
+        mListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mListItems);
+        mListView.setAdapter(mListAdapter);
+
+        // copy the card database into an accessible location
+        String cardFile = "card.db";
+        String filename = getFilesDir() + File.separator + cardFile;
+        try {
+            // check if the file exists first
+            File f = new File(filename);
+            if (!f.exists()) {
+                // copy the card db file so it is accessible by native applications
+                InputStream fis = getResources().openRawResource(R.raw.card);
+                FileOutputStream fos = openFileOutput(cardFile, Context.MODE_PRIVATE);
+                int bytesRead = 0;
+                do {
+                    byte[] data = new byte[1024];
+                    bytesRead = fis.read(data);
+                    if (bytesRead == -1 || bytesRead == 0) {
+                        break;
+                    }
+                    fos.write(data);
+                } while (true);
+                Toast.makeText(this, "Written file " + filename, Toast.LENGTH_LONG).show();
+            }
+            // setup the database
+            DB.setPath(filename);
+
+            // test round trip
+            String s = DB.getPath();
+            TextView textView = (TextView) findViewById(R.id.textView);
+            textView.setText(s);
+        } catch (FileNotFoundException e) {
+            Toast.makeText(this, "Can't open " + filename, Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            Toast.makeText(this, "Error reading " + filename, Toast.LENGTH_LONG).show();
+        }
+
     }
 
     private View.OnClickListener mOnIncrement = new View.OnClickListener() {
 
         @Override
         public void onClick(View v) {
-            mCounter = mExample.increment(mCounter);
-            Toast.makeText(MainActivity.this, Integer.toString(mCounter), Toast.LENGTH_SHORT).show();
+            // search for the cards
+            CardSelector cs = new CardSelector();
+            cs.name("Black");
+            ArrayList<String> ret = cs.execute();
+            mListAdapter.clear();
+            mListAdapter.addAll(ret);
+            mListAdapter.notifyDataSetChanged();
         }
     };
 
