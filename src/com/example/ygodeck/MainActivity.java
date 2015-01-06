19ae9326c09537a4ed82663447b52d1ad5722c1c
Deon Poncini <dex1337@gmail.com>
2015-01-05 22:11:51 -0800

    Start example android app with login screen

--- src/com/example/ygodeck/MainActivity.java
+++ src/com/example/ygodeck/MainActivity.java
@@ -3,28 +3,21 @@ package com.example.ygodeck;
 import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
 
-import net.sectorsoftware.ygo.data.DataTypes;
 import net.sectorsoftware.ygo.deck.DB;
-import net.sectorsoftware.ygo.deck.CardSelector;
+import net.sectorsoftware.ygo.deck.User;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -32,25 +25,73 @@ public class MainActivity extends ActionBarActivity {
         System.loadLibrary("ygodeck-jni");
     }
 
-    private ListView mListView;
-    List<String> mListItems = new ArrayList<String>();
-    ArrayAdapter<String> mListAdapter;
+    private EditText mUserNameText;
+    private User mUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button increment = (Button) findViewById(R.id.button);
-        increment.setOnClickListener(mOnIncrement);
+        loadCardFile();
 
-        Button query = (Button) findViewById(R.id.query);
-        query.setOnClickListener(mOnQuery);
+        Button userLogin = (Button) findViewById(R.id.button_user_login);
+        userLogin.setOnClickListener(mOnUserLogin);
 
-        mListView = (ListView) findViewById(R.id.listView);
-        mListAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mListItems);
-        mListView.setAdapter(mListAdapter);
+        Button userAdd = (Button) findViewById(R.id.button_user_add);
+        userAdd.setOnClickListener(mOnUserAdd);
 
+        Button userRemove = (Button) findViewById(R.id.button_user_remove);
+        userRemove.setOnClickListener(mOnUserRemove);
+
+        mUserNameText = (EditText) findViewById(R.id.text_user_name);
+
+    }
+
+    private View.OnClickListener mOnUserLogin = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            String text = mUserNameText.getText().toString();
+            if (text.isEmpty()) {
+                Toast.makeText(MainActivity.this, "Please enter a username", Toast.LENGTH_SHORT).show();
+            } else {
+                mUser = new User(text);
+            }
+        }
+    };
+
+    private View.OnClickListener mOnUserAdd = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            String text = mUserNameText.getText().toString();
+            if (text.isEmpty()) {
+                Toast.makeText(MainActivity.this, "Please enter a username", Toast.LENGTH_SHORT).show();
+            } else {
+                mUser = new User(text, true);
+            }
+        }
+    };
+
+    private View.OnClickListener mOnUserRemove = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            String text = mUserNameText.getText().toString();
+            if (text.isEmpty()) {
+                Toast.makeText(MainActivity.this, "Please enter a username", Toast.LENGTH_SHORT).show();
+            } else {
+                mUser = new User(text);
+                mUser.remove();
+                mUser.delete();
+            }
+        }
+    };
+
+    private void loadCardFile()
+    {
         // copy the card database into an accessible location
         String cardFile = "card.db";
         String filename = getFilesDir() + File.separator + cardFile;
@@ -74,61 +115,12 @@ public class MainActivity extends ActionBarActivity {
             }
             // setup the database
             DB.setPath(filename);
-
-            // test round trip
-            String s = DB.getPath();
-            TextView textView = (TextView) findViewById(R.id.textView);
-            textView.setText(s);
         } catch (FileNotFoundException e) {
             Toast.makeText(this, "Can't open " + filename, Toast.LENGTH_LONG).show();
         } catch (IOException e) {
             Toast.makeText(this, "Error reading " + filename, Toast.LENGTH_LONG).show();
         }
-
     }
-
-    private View.OnClickListener mOnIncrement = new View.OnClickListener() {
-
-        @Override
-        public void onClick(View v) {
-            // search for the cards
-            CardSelector cs = new CardSelector();
-            cs.name("Black");
-            List<String> ret = cs.execute();
-            mListAdapter.clear();
-            mListAdapter.addAll(ret);
-            mListAdapter.notifyDataSetChanged();
-        }
-    };
-
-    private View.OnClickListener mOnQuery = new View.OnClickListener() {
-
-        @Override
-        public void onClick(View v) {
-            // search for the cards
-            CardSelector cs = new CardSelector();
-            DataTypes.StaticCardData scd = cs.query("Sangan");
-            cs.delete();
-
-            mListAdapter.clear();
-            mListAdapter.add(scd.name);
-            mListAdapter.add(scd.cardType.name());
-            mListAdapter.add(scd.attribute.name());
-            mListAdapter.add(scd.monsterType.name());
-            mListAdapter.add(scd.type.name());
-            mListAdapter.add(scd.monsterAbility.name());
-            mListAdapter.add(Integer.toString(scd.level));
-            mListAdapter.add(Integer.toString(scd.attack));
-            mListAdapter.add(Integer.toString(scd.defense));
-            mListAdapter.add(Integer.toString(scd.lpendulum));
-            mListAdapter.add(Integer.toString(scd.rpendulum));
-            mListAdapter.add(scd.spellType.name());
-            mListAdapter.add(scd.trapType.name());
-            mListAdapter.add(scd.text);
-            mListAdapter.notifyDataSetChanged();
-            scd.cardType.values();
-        }
-    };
 
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
